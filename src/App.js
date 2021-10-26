bc4b0f67d5df035513172c4745051f9cfc2981cd
colekins <colekins@gmail.com>
2021-10-26 17:45:58 -0400

    Clean up, new favicon

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
     <div className='App'>
       <header className='header'>
         <h1>Read the Green New Deal</h1>
+        <p>Coming soon...</p>
       </header>
-      <div className='main'>kjhkjhjkhkjhjkhj</div>
+      <div className='main'></div>
     </div>
   );
 }
