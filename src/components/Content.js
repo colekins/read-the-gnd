c57e5cc86a6094e25bdb927969a81749cf2999c9
colekins <colekins@gmail.com>
2021-10-28 19:27:14 -0400

    Start adding text

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,64 +2,204 @@ import './content.css';
 
 const Content = () => (
   <div className='content'>
+    <h2 className='bill-title'>H. RES. 109</h2>
+    <div id='intro'>
+      <p>
+        Recognizing the duty of the Federal Government to create a Green New
+        Deal.
+      </p>
+      <p>February 7, 2019</p>
+      <p>
+        Ms. Ocasio-Cortez (for herself, Mr. Hastings, Ms. Tlaib, Mr. Serrano,
+        Mrs. Carolyn B. Maloney of New York, Mr. Vargas, Mr. Espaillat, Mr.
+        Lynch, Ms. Velázquez, Mr. Blumenauer, Mr. Brendan F. Boyle of
+        Pennsylvania, Mr. Castro of Texas, Ms. Clarke of New York, Ms. Jayapal,
+        Mr. Khanna, Mr. Ted Lieu of California, Ms. Pressley, Mr. Welch, Mr.
+        Engel, Mr. Neguse, Mr. Nadler, Mr. McGovern, Mr. Pocan, Mr. Takano, Ms.
+        Norton, Mr. Raskin, Mr. Connolly, Mr. Lowenthal, Ms. Matsui, Mr.
+        Thompson of California, Mr. Levin of California, Ms. Pingree, Mr.
+        Quigley, Mr. Huffman, Mrs. Watson Coleman, Mr. García of Illinois, Mr.
+        Higgins of New York, Ms. Haaland, Ms. Meng, Mr. Carbajal, Mr. Cicilline,
+        Mr. Cohen, Ms. Clark of Massachusetts, Ms. Judy Chu of California, Ms.
+        Mucarsel-Powell, Mr. Moulton, Mr. Grijalva, Mr. Meeks, Mr. Sablan, Ms.
+        Lee of California, Ms. Bonamici, Mr. Sean Patrick Maloney of New York,
+        Ms. Schakowsky, Ms. DeLauro, Mr. Levin of Michigan, Ms. McCollum, Mr.
+        DeSaulnier, Mr. Courtney, Mr. Larson of Connecticut, Ms. Escobar, Mr.
+        Schiff, Mr. Keating, Mr. DeFazio, Ms. Eshoo, Mrs. Trahan, Mr. Gomez, Mr.
+        Kennedy, and Ms. Waters) submitted the following resolution; which was
+        referred to the Committee on Energy and Commerce, and in addition to the
+        Committees on Science, Space, and Technology, Education and Labor,
+        Transportation and Infrastructure, Agriculture, Natural Resources,
+        Foreign Affairs, Financial Services, the Judiciary, Ways and Means, and
+        Oversight and Reform, for a period to be subsequently determined by the
+        Speaker, in each case for consideration of such provisions as fall
+        within the jurisdiction of the committee concerned
+      </p>
+    </div>
+    <h1 className='bill-title'>RESOLUTION</h1>
+
     <p>
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-      nulla pariatur?"
+      Recognizing the duty of the Federal Government to create a Green New Deal.
     </p>
     <p>
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-      praesentium voluptatum deleniti atque corrupti quos dolores et quas
-      molestias excepturi sint occaecati cupiditate non provident, similique
-      sunt in culpa qui officia deserunt mollitia animi, id est laborum et
-      dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-      Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-      impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
-      assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut
-      officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-      repudiandae sint et molestiae non recusandae. Itaque earum rerum hic
-      tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias
-      consequatur aut perferendis doloribus asperiores repellat."
+      Whereas the October 2018 report entitled “Special Report on Global Warming
+      of 1.5 ºC” by the Intergovernmental Panel on Climate Change and the
+      November 2018 Fourth National Climate Assessment report found that—
+    </p>
+    <ol>
+      <li>
+        human activity is the dominant cause of observed climate change over the
+        past century;
+      </li>
+      <li>
+        a changing climate is causing sea levels to rise and an increase in
+        wildfires, severe storms, droughts, and other extreme weather events
+        that threaten human life, healthy communities, and critical
+        infrastructure;
+      </li>
+      <li>
+        global warming at or above 2 degrees Celsius beyond preindustrialized
+        levels will cause—
+        <ol type='a'>
+          <li>
+            mass migration from the regions most affected by climate change;
+          </li>
+          <li>
+            more than $500,000,000,000 in lost annual economic output in the
+            United States by the year 2100;
+          </li>
+          <li>
+            wildfires that, by 2050, will annually burn at least twice as much
+            forest area in the western United States than was typically burned
+            by wildfires in the years preceding 2019;
+          </li>
+          <li>a loss of more than 99 percent of all coral reefs on Earth;</li>
+          <li>
+            more than 350,000,000 more people to be exposed globally to deadly
+            heat stress by 2050; and
+          </li>
+          <li>
+            a risk of damage to $1,000,000,000,000 of public infrastructure and
+            coastal real estate in the United States; and
+          </li>
+        </ol>
+      </li>
+      <li>
+        global temperatures must be kept below 1.5 degrees Celsius above
+        preindustrialized levels to avoid the most severe impacts of a changing
+        climate, which will require—
+        <ol type='a'>
+          <li>
+            global reductions in greenhouse gas emissions from human sources of
+            40 to 60 percent from 2010 levels by 2030; and
+          </li>
+          <li>net-zero global emissions by 2050;</li>
+        </ol>
+      </li>
+    </ol>
+    <p>
+      Whereas, because the United States has historically been responsible for a
+      disproportionate amount of greenhouse gas emissions, having emitted 20
+      percent of global greenhouse gas emissions through 2014, and has a high
+      technological capacity, the United States must take a leading role in
+      reducing emissions through economic transformation;
     </p>
     <p>
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-      nulla pariatur?"
+      Whereas the United States is currently experiencing several related
+      crises, with—
+    </p>
+    <ol>
+      <li>
+        life expectancy declining while basic needs, such as clean air, clean
+        water, healthy food, and adequate health care, housing, transportation,
+        and education, are inaccessible to a significant portion of the United
+        States population;
+      </li>
+      <li>
+        a 4-decade trend of wage stagnation, deindustrialization, and antilabor
+        policies that has led to—
+        <ol type='a'>
+          <li>
+            hourly wages overall stagnating since the 1970s despite increased
+            worker productivity;
+          </li>
+          <li>
+            the third-worst level of socioeconomic mobility in the developed
+            world before the Great Recession;
+          </li>
+          <li>
+            the erosion of the earning and bargaining power of workers in the
+            United States; and
+          </li>
+          <li>
+            {' '}
+            inadequate resources for public sector workers to confront the
+            challenges of climate change at local, State, and Federal levels;
+            and
+          </li>
+        </ol>
+      </li>
+      <li>
+        the greatest income inequality since the 1920s, with—
+        <ol type='a'>
+          <li>
+            the top 1 percent of earners accruing 91 percent of gains in the
+            first few years of economic recovery after the Great Recession;
+          </li>
+          <li>
+            a large racial wealth divide amounting to a difference of 20 times
+            more wealth between the average white family and the average black
+            family; and
+          </li>
+          <li>
+            a gender earnings gap that results in women earning approximately 80
+            percent as much as men, at the median;
+          </li>
+        </ol>
+      </li>
+    </ol>
+    <p>
+      Whereas climate change, pollution, and environmental destruction have
+      exacerbated systemic racial, regional, social, environmental, and economic
+      injustices (referred to in this preamble as “systemic injustices”) by
+      disproportionately affecting indigenous peoples, communities of color,
+      migrant communities, deindustrialized communities, depopulated rural
+      communities, the poor, low-income workers, women, the elderly, the
+      unhoused, people with disabilities, and youth (referred to in this
+      preamble as “frontline and vulnerable communities”);
     </p>
     <p>
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-      praesentium voluptatum deleniti atque corrupti quos dolores et quas
-      molestias excepturi sint occaecati cupiditate non provident, similique
-      sunt in culpa qui officia deserunt mollitia animi, id est laborum et
-      dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-      Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-      impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
-      assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut
-      officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-      repudiandae sint et molestiae non recusandae. Itaque earum rerum hic
-      tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias
-      consequatur aut perferendis doloribus asperiores repellat."
+      Whereas, climate change constitutes a direct threat to the national
+      security of the United States—
     </p>
+    <ol>
+      <li>
+        by impacting the economic, environmental, and social stability of
+        countries and communities around the world; and
+      </li>
+      <li>by acting as a threat multiplier;</li>
+    </ol>
+    <p>
+      Whereas the Federal Government-led mobilizations during World War II and
+      the New Deal created the greatest middle class that the United States has
+      ever seen, but many members of frontline and vulnerable communities were
+      excluded from many of the economic and societal benefits of those
+      mobilizations; and
+    </p>
+    <p>
+      Whereas the House of Representatives recognizes that a new national,
+      social, industrial, and economic mobilization on a scale not seen since
+      World War II and the New Deal era is a historic opportunity—
+    </p>
+    <ol>
+      <li> to create millions of good, high-wage jobs in the United States;</li>
+      <li>
+        to provide unprecedented levels of prosperity and economic security for
+        all people of the United States; and
+      </li>
+      <li>to counteract systemic injustices: Now, therefore, be it</li>
+    </ol>
+    <p>Resolved, That it is the sense of the House of Representatives that—</p>
   </div>
 );
 
