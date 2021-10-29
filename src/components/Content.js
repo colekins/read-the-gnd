a21a213921df714143a66dd0fe48eb4196597ce3
colekins <colekins@gmail.com>
2021-10-28 20:00:40 -0400

    More text

--- src/components/Content.js
+++ src/components/Content.js
@@ -200,6 +200,193 @@ const Content = () => (
       <li>to counteract systemic injustices: Now, therefore, be it</li>
     </ol>
     <p>Resolved, That it is the sense of the House of Representatives that—</p>
+    <ol>
+      <li>
+        it is the duty of the Federal Government to create a Green New Deal—
+        <ol type='a'>
+          <li>
+            to achieve net-zero greenhouse gas emissions through a fair and just
+            transition for all communities and workers;
+          </li>
+
+          <li>
+            to create millions of good, high-wage jobs and ensure prosperity and
+            economic security for all people of the United States;
+          </li>
+
+          <li>
+            to invest in the infrastructure and industry of the United States to
+            sustainably meet the challenges of the 21st century;
+          </li>
+
+          <li>
+            to secure for all people of the United States for generations to
+            come—
+            <ol type='i'>
+              <li>clean air and water;</li>
+
+              <li>climate and community resiliency;</li>
+
+              <li>healthy food;</li>
+
+              <li>access to nature; and</li>
+
+              <li>a sustainable environment; and</li>
+            </ol>
+          </li>
+          <li>
+            to promote justice and equity by stopping current, preventing
+            future, and repairing historic oppression of indigenous peoples,
+            communities of color, migrant communities, deindustrialized
+            communities, depopulated rural communities, the poor, low-income
+            workers, women, the elderly, the unhoused, people with disabilities,
+            and youth (referred to in this resolution as “frontline and
+            vulnerable communities”);
+          </li>
+        </ol>
+      </li>
+      <li>
+        the goals described in subparagraphs (A) through (E) of paragraph (1)
+        (referred to in this resolution as the “Green New Deal goals”) should be
+        accomplished through a 10-year national mobilization (referred to in
+        this resolution as the “Green New Deal mobilization”) that will require
+        the following goals and projects—
+        <ol type='a'>
+          <li>
+            building resiliency against climate change-related disasters, such
+            as extreme weather, including by leveraging funding and providing
+            investments for community-defined projects and strategies;
+          </li>
+          <li>
+            repairing and upgrading the infrastructure in the United States,
+            including—
+            <ol type='i'>
+              <li>
+                by eliminating pollution and greenhouse gas emissions as much as
+                technologically feasible;
+              </li>
+
+              <li>by guaranteeing universal access to clean water;</li>
+
+              <li>by reducing the risks posed by climate impacts; and</li>
+
+              <li>
+                by ensuring that any infrastructure bill considered by Congress
+                addresses climate change;
+              </li>
+            </ol>
+          </li>
+          <li>
+            meeting 100 percent of the power demand in the United States through
+            clean, renewable, and zero-emission energy sources, including—
+            <ol type='i'>
+              <li>
+                by dramatically expanding and upgrading renewable power sources;
+                and
+              </li>
+
+              <li>by deploying new capacity;</li>
+            </ol>
+          </li>
+          <li>
+            building or upgrading to energy-efficient, distributed, and “smart”
+            power grids, and ensuring affordable access to electricity;
+          </li>
+
+          <li>
+            upgrading all existing buildings in the United States and building
+            new buildings to achieve maximum energy efficiency, water
+            efficiency, safety, affordability, comfort, and durability,
+            including through electrification;
+          </li>
+
+          <li>
+            spurring massive growth in clean manufacturing in the United States
+            and removing pollution and greenhouse gas emissions from
+            manufacturing and industry as much as is technologically feasible,
+            including by expanding renewable energy manufacturing and investing
+            in existing manufacturing and industry;
+          </li>
+          <li>
+            working collaboratively with farmers and ranchers in the United
+            States to remove pollution and greenhouse gas emissions from the
+            agricultural sector as much as is technologically feasible,
+            including—
+            <ol type='i'>
+              <li>by supporting family farming;</li>
+
+              <li>
+                by investing in sustainable farming and land use practices that
+                increase soil health; and
+              </li>
+
+              <li>
+                by building a more sustainable food system that ensures
+                universal access to healthy food;
+              </li>
+            </ol>
+          </li>
+          <li>
+            overhauling transportation systems in the United States to remove
+            pollution and greenhouse gas emissions from the transportation
+            sector as much as is technologically feasible, including through
+            investment in—
+            <ol type='i'>
+              <li>zero-emission vehicle infrastructure and manufacturing;</li>
+
+              <li>clean, affordable, and accessible public transit; and</li>
+
+              <li>high-speed rail;</li>
+            </ol>
+          </li>
+          <li>
+            mitigating and managing the long-term adverse health, economic, and
+            other effects of pollution and climate change, including by
+            providing funding for community-defined projects and strategies;
+          </li>
+
+          <li>
+            removing greenhouse gases from the atmosphere and reducing pollution
+            by restoring natural ecosystems through proven low-tech solutions
+            that increase soil carbon storage, such as land preservation and
+            afforestation;
+          </li>
+
+          <li>
+            restoring and protecting threatened, endangered, and fragile
+            ecosystems through locally appropriate and science-based projects
+            that enhance biodiversity and support climate resiliency;
+          </li>
+
+          <li>
+            cleaning up existing hazardous waste and abandoned sites, ensuring
+            economic development and sustainability on those sites;
+          </li>
+
+          <li>
+            identifying other emission and pollution sources and creating
+            solutions to remove them; and
+          </li>
+
+          <li>
+            promoting the international exchange of technology, expertise,
+            products, funding, and services, with the aim of making the United
+            States the international leader on climate action, and to help other
+            countries achieve a Green New Deal;
+          </li>
+        </ol>
+      </li>
+      <li>
+        a Green New Deal must be developed through transparent and inclusive
+        consultation, collaboration, and partnership with frontline and
+        vulnerable communities, labor unions, worker cooperatives, civil society
+        groups, academia, and businesses; and
+      </li>
+      <li>
+        to achieve the Green New Deal goals and mobilization, a Green New Deal
+        will require the following goals and projects—
+      </li>
+    </ol>
   </div>
 );
 
